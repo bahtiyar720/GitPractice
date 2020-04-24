18a40b92d2833a83ae05f44a55822ce594d57429
bahtiyar720 <bahtiyar720@gmail.com>
2020-04-24 12:26:37 -0700

    add change

--- src/Day01_/AllGood.java
+++ src/Day01_/AllGood.java
@@ -3,5 +3,7 @@ package Day01_;
 public class AllGood {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        System.out.println("hello bahtiyar");
+        System.out.println("hello bahit");
     }
 }
