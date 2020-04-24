b6bec703516ddd5df086bbf48e5e559d1c359535
bahtiyar720 <bahtiyar720@gmail.com>
2020-04-24 13:47:16 -0700

    add deleted

--- src/bahiyar.java
+++
@@ -1,7 +0,0 @@
-public class AllGood {
-    public static void main(String[] args) {
-        System.out.println("Hello Github");
-        System.out.println("hello bahtiyar");
-        System.out.println("hello bahit");
-    }
-}
