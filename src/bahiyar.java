2d6d333a61ce359db880f5e262e4e4cd422681db
bahtiyar720 <64111053+bahtiyar720@users.noreply.github.com>
2020-04-24 13:45:07 -0700

    Create bahiyar.java
    file created form git hub

---
+++ src/bahiyar.java
@@ -0,0 +1,7 @@
+public class AllGood {
+    public static void main(String[] args) {
+        System.out.println("Hello Github");
+        System.out.println("hello bahtiyar");
+        System.out.println("hello bahit");
+    }
+}
